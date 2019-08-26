ac96c91b4971b98213751a1683e8f1652da30149
Alesha <C:\Users\\AppData\Roaming\The Bat!\Mail>
2019-08-26 17:03:19 +0300

    commit

---
+++ flaxxball/js/slider.js
@@ -0,0 +1,30 @@
+var slider_index=1;
+
+function f_sliderclickleft(){
+
+    if (slider_index > 1)
+    {
+      element = document.getElementById("slider" + slider_index);
+      element.classList.remove("slick-active");
+      slider_index = slider_index - 1;
+      element = document.getElementById("slider"  + slider_index);
+      element.classList.add("slick-active");
+      
+    }
+}
+
+function f_sliderclickright(){
+    var element;
+
+    if (slider_index < 3)
+    {
+      element = document.getElementById("slider" + slider_index);
+      element.classList.remove("slick-active");
+      slider_index = slider_index + 1;
+      element = document.getElementById("slider"  + slider_index);
+      element.classList.add("slick-active");
+      
+    }
+}
+
+
